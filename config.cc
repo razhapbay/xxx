d249e67c16f3b08a2f6c5d264375888db618e1e0
razhapbay <97940939+razhapbay@users.noreply.github.com>
2024-03-27 03:25:30 -0400

    Add files via upload

---
+++ config.cc
@@ -0,0 +1,13 @@
+[common]
+server_addr = 154.90.62.240
+server_port = 443
+token = xxx
+
+[plugin_socks5_withpassword]
+type = tcp
+remote_port = 8081
+plugin = socks5
+use_encryption = true
+use_compression = true
+plugin_user = socks_user
+plugin_passwd = socks_password!!!
